a6213131e39a8be933ef0cfd944bdab88a917946
lapislui <lapislui1234@gmail.com>
2025-02-13 18:33:18 +0530

    updating

--- level_two/level_two/settings.py
+++ level_two/level_two/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d2yo1-g&(^h0zwo*%_iur6_ezevybtwf@@1qmd2h=(k6)24y_z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
