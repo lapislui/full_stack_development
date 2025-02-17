519257d96950b9921d4fcce256d21f49dbc77087
lapislui <lapislui1234@gmail.com>
2025-02-17 14:08:41 +0530

    Add navigation bar to index page and create relative URL template

--- level_two/first_app/urls.py
+++ level_two/first_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from first_app import views
 
+app_name = 'first_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('form/', views.form_name_view, name='form_name_view'),
