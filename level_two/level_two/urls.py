ac5a70e3306f04819af127296e819fb2ad8de66a
lapislui <lapislui1234@gmail.com>
2025-02-18 13:32:53 +0530

    Comment out index view path in URL configuration

--- level_two/level_two/urls.py
+++ level_two/level_two/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from first_app import views
+# from first_app import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path('first_app/', include('first_app.urls')),
     # path('form/', include('first_app.urls')),
     path('admin/', admin.site.urls),
